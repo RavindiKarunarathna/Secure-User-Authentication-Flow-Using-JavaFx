4e5d1a599c2d5b963df30db2478182a2477b0284
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-13 13:23:42 +0530

    Edit LoginDTO class

--- src/main/java/model/dto/LoginDTO.java
+++ src/main/java/model/dto/LoginDTO.java
@@ -1,5 +1,14 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginDTO {
-    
+    private String email;
+
+    private String password;
 }
