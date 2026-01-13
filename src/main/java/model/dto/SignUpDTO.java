411f3f43a3022da3c16b64c9ed671704e55e9cf0
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-13 13:40:05 +0530

    Edit SignUpDTO class

--- src/main/java/model/dto/SignUpDTO.java
+++ src/main/java/model/dto/SignUpDTO.java
@@ -1,4 +1,19 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class SignUpDTO {
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
 }
