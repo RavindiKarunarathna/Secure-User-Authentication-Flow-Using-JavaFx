3a673f3e753cf101a207ce5b854170dcea2c080d
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-13 11:59:39 +0530

    Edit WelcomeForm.fxml file & WelcomeFormController

--- src/main/java/controller/welcomeController/WelcomeFormController.java
+++ src/main/java/controller/welcomeController/WelcomeFormController.java
@@ -1,5 +1,17 @@
 package controller.welcomeController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class WelcomeFormController {
-    
+
+    @FXML
+    private AnchorPane txtFirstName;
+
+    @FXML
+    void btnOnActionLogOut(ActionEvent event) {
+
+    }
+
 }
