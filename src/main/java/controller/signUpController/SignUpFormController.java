a1b8ac3b254f334b94bcc2f5ca187fa36ff5d31a
RavindiKarunarathna <ravindiicet@gmail.com>
2026-01-13 11:01:32 +0530

    Edit SignUpForm.fxml file & SignUpFormController

--- src/main/java/controller/signUpController/SignUpFormController.java
+++ src/main/java/controller/signUpController/SignUpFormController.java
@@ -1,5 +1,34 @@
 package controller.signUpController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpFormController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtReEnterPassword;
+
+    @FXML
+    void btnOnActionBacktoLogin(ActionEvent event) {
+
+    }
+    @FXML
+    void btnOnActionRegister(ActionEvent event) {
+
+    }
 
 }
